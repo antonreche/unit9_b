2e00d39c7cb00b4bd6edb49d22981b70ae3ae80a
antonreche <antonreche@gmail.com>
2015-05-12 00:07:26 +0200

    test1ValidaNif -->Caso 1 del método validaNif

---
+++ src/unit9_b/AlumnosTest.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package unit9_b;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Clase AlumnoTest creada por JUnit para testear la clase alumno
+ * @author Antonio Reche Ramal
+ */
+public class AlumnosTest {
+	
+	private Alumno alumno;	
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		alumno=new Alumno();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link unit9_b.Alumno#validaNif(java.lang.String)}.
+	 * Testa para el Caso 1, la longitud del nif no es de 9 caracteres.
+	 */
+	@Test
+	public void test1ValidaNif() {
+		String nif="12345678";
+		
+		assertFalse(alumno.validaNif(nif));
+	}
+
+	/**
+	 * Test method for {@link unit9_b.Alumno#calculaTasaMatricula(int, boolean, boolean)}.
+	 *
+	@Test
+	public void testCalculaTasaMatricula() {
+		fail("Not yet implemented");
+	}
+*/
+}
